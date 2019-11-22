4d8e055174963aa6d2f167aa4c1405700aab4ea1
Alluka <hsiaoyeung.lee@gmail.com>
2019-11-22 15:17:31 +0800

    gen name cha rnn

--- NLP/gen_names_cha_rnn.py
+++ NLP/gen_names_cha_rnn.py
@@ -8,6 +8,9 @@ import torch.nn as nn
 import torch
 import random
 import time
+import math
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 # Preparing the data
 all_letters = string.ascii_letters + " .,;'-"
@@ -54,6 +57,7 @@ if n_categories == 0:
 
 print('# categories:', n_categories, all_categories)
 print(unicode2ascii("O'Néàl"))
+print(n_letters)
 
 
 # Creating the Network
@@ -62,9 +66,9 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
 
-        self.i2h = nn.Linear(n_categories + input_size + output_size, hidden_size)
-        self.i2o = nn.Linear(n_categories + input_size + output_size, output_size)
-        self.o2o = nn.Linear(input_size + output_size, output_size)
+        self.i2h = nn.Linear(n_categories + input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(n_categories + input_size + hidden_size, output_size)
+        self.o2o = nn.Linear(hidden_size + output_size, output_size)
         self.dropout = nn.Dropout(0.1)
         self.softmax = nn.LogSoftmax(dim=1)
 
@@ -119,7 +123,6 @@ def input2tensor(line_input):
         tensor[i][0][all_letters.find(letter)] = 1
     return tensor
 
-import numpy as np
 
 def target2tensor(line_input):
     """
@@ -127,7 +130,7 @@ def target2tensor(line_input):
     """
     letter_indexes = [all_letters.find(line_input[i]) for i in range(1, len(line_input))]
     letter_indexes.append(n_letters - 1)   # EOS
-    return torch.LongTensor(1)
+    return torch.LongTensor(letter_indexes)
 
 
 def ran_sample():
@@ -142,3 +145,102 @@ def ran_sample():
 
 
 # Training the Network
+criterion = nn.NLLLoss()
+learning_rate = 0.0005
+rnn = RNN(n_letters, 128, n_letters)
+
+
+def train(category_tensor, input_tensor, target_tensor):
+    target_tensor.unsqueeze_(-1)
+    hidden = rnn.init_hidden()
+
+    rnn.zero_grad()
+
+    loss = 0
+
+    for i in range(input_tensor.size(0)):
+        output, hidden = rnn(category_tensor, input_tensor[i], hidden)
+        l = criterion(output, target_tensor[i])
+        loss += l
+
+    loss.backward()
+
+    for p in rnn.parameters():
+        p.data.add_(-learning_rate, p.grad.data)
+
+    return output, loss.item() / input_tensor.size(0)
+
+
+def timesince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+n_iters = 100000
+print_every = 5000
+plot_every = 500
+all_losses = []
+total_loss = 0
+
+start = time.time()
+
+for iter in range(1, n_iters + 1):
+    output, loss = train(*ran_sample())
+    total_loss += loss
+
+    if iter % print_every == 0:
+        print('%s (%d %d%%) %.4f' % (timesince(start), iter, iter / n_iters * 100, loss))
+
+    if iter % plot_every == 0:
+        all_losses.append(total_loss / plot_every)
+        total_loss = 0
+
+# Plotting the Losses
+plt.figure()
+plt.plot(all_losses)
+
+# Sampling the Network
+max_length = 20
+
+
+def sample(category_sample, start_letter='A'):
+    """
+    Sample from a category and starting letter
+    """
+    with torch.no_grad():
+        category_tensor = category2tensor(category_sample)
+        input_tensor = input2tensor(start_letter)
+        hidden = rnn.init_hidden()
+
+        output_name = start_letter
+
+        for i in range(max_length):
+            output, hidden = rnn(category_tensor, input_tensor[0], hidden)
+            topv, topi = output.topk(1)
+            topi = topi[0][0]
+            if topi == n_letters - 1:
+                break
+            else:
+                letter = all_letters[topi]
+                output_name += letter
+            input_tensor = input2tensor(letter)
+
+        return output_name
+
+
+def samples(category_sample, start_letters='ABC'):
+    """
+    Get multiple samples from one category and multiple starting letters
+    """
+    for start_letter in start_letters:
+        print(sample(category_sample, start_letter))
+
+
+samples('Russian', 'RUS')
+
+samples('German', 'GER')
+
+samples('Chinese', 'LCI')
